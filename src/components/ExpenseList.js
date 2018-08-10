ab41385e639c55ae78fcd38991cd99292e033600
EDWARD <edsztukowski@gmail.com>
2018-08-09 18:03:52 -0600

    stable push

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,16 +4,26 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-          props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />;
-          })
-        )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-mobile">Expenses</div>
+      <div className="show-desktop">Expenses</div>
+      <div className="show-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item--message">
+            <span>No expenses</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
+    
   </div>
 );
 
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -3,14 +3,19 @@ import ExpensesTotalSelector from '../selectors/expenses-total';
 import ExpensesFilterSelector from '../selectors/expenses';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
+import {Link} from 'react-router-dom';
 
 export const ExpensesSummary = (props) => {
     const expenseWord = props.expensesCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('0,0.00')
     return (
-        <div>
-            <p>{expenseWord} Count: {props.expensesCount}</p>
-            <p>{expenseWord} : ${formattedExpensesTotal} </p>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expensesCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button__blue" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     )
 }
